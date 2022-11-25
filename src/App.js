9b00d7db172b695b2d637f79e6b4d37e88252a34
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-25 20:06:35 +0600

    route setup done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-active btn-secondary">Button</button>
-      <button className="btn btn-active btn-accent">Button</button>
-      <button className="btn btn-active btn-ghost">Button</button>
-      <button className="btn btn-active btn-link">Button</button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
