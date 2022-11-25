03f71c20db45e544fd14056631c32140c8fdf15c
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-25 21:07:46 +0600

    carousel added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Carousel from './Carousel/Carousel';
 
 const Home = () => {
     return (
         <div>
-            <h2>THis is Home</h2>
+            <Carousel></Carousel>
         </div>
     );
 };
--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
               <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-                <div class="relative flex items-center justify-between">
+                <div class="relative absolute z-20  flex items-center justify-between">
                   <a
                     href="/"
                     aria-label="Company"
